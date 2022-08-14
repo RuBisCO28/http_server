036b6e72c3f14e2794d8835730b5a63e55b7a1ff
RuBiCO28 <54470624+RuBisCO28@users.noreply.github.com>
2022-08-14 17:43:03 +0900

    dynamic_html

--- workerthread.py
+++ workerthread.py
@@ -2,8 +2,11 @@ import socket
 from datetime import datetime
 import os
 import traceback
-from typing import Tuple
+from typing import Tuple, Optional
 from threading import Thread
+import textwrap
+from pprint import pformat
+import re
 
 class WorkerThread(Thread):
   BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -35,17 +38,60 @@ class WorkerThread(Thread):
 
       method, path, http_version, request_header, request_body = self.parse_http_request(request)
 
-      try:
-        # generate response body
-        response_body = self.get_static_file_content(path)
+      response_body: bytes
+      content_type: Optional[str]
+      response_line: str
 
-        # generate response line
+      if path == "/now":
+        html = f"""\
+              <html>
+              <body>
+                  <h1>Now: {datetime.now()}</h1>
+              </body>
+              </html>
+        """
+        response_body = textwrap.dedent(html).encode()
+
+        content_type = "text/html"
         response_line = "HTTP/1.1 200 OK\r\n"
-      except OSError:
-        response_body = b"<html><body><h1>404 Not Found</h1></body></html>"
-        response_line = "HTTP/1.1 404 Not Found\r\n"
 
-      response_header = self.build_response_header(path, response_body)
+      elif path == "/show_request":
+        html = f"""\
+            <html>
+            <body>
+                <h1>Request Line:</h1>
+                <p>
+                    {method} {path} {http_version}
+                </p>
+                <h1>Headers:</h1>
+                <pre>{pformat(request_header)}</pre>
+                <h1>Body:</h1>
+                <pre>{request_body.decode("utf-8", "ignore")}</pre>
+
+            </body>
+            </html>
+        """
+        response_body = textwrap.dedent(html).encode()
+
+        content_type = "text/html"
+
+        response_line = "HTTP/1.1 200 OK\r\n"
+
+      else:
+        try:
+          # generate response body
+          response_body = self.get_static_file_content(path)
+
+          content_type = None
+
+          # generate response line
+          response_line = "HTTP/1.1 200 OK\r\n"
+        except OSError:
+          response_body = b"<html><body><h1>404 Not Found</h1></body></html>"
+          content_type = "text/html"
+          response_line = "HTTP/1.1 404 Not Found\r\n"
+
+      response_header = self.build_response_header(path, response_body, content_type)
 
       response = (response_line + response_header + "\r\n").encode() + response_body
 
@@ -61,14 +107,18 @@ class WorkerThread(Thread):
       print(f"=== Worker: Disconnect remote_address:{self.client_address} ===")
       self.client_socket.close()
 
-  def parse_http_request(self, request: bytes) -> Tuple[str, str, str, bytes, bytes]:
+  def parse_http_request(self, request: bytes) -> Tuple[str, str, str, dict, bytes]:
     request_line, remain = request.split(b"\r\n",maxsplit=1)
     request_header, request_body = remain.split(b"\r\n\r\n", maxsplit=1)
 
     # parse request_line
     method, path, http_version = request_line.decode().split(" ")
 
-    return  method, path, http_version, request_header, request_body
+    headers = {}
+    for header_now in request_header.decode().split("\r\n"):
+      key, value = re.split(r": *", header_now, maxsplit=1)
+      headers[key] = value
+    return method, path, http_version, headers, request_body
 
   def get_static_file_content(self, path: str) -> bytes:
     # get static file path
@@ -79,12 +129,13 @@ class WorkerThread(Thread):
     with open(static_file_path, "rb") as f:
       return f.read()
 
-  def build_response_header(self, path: str, response_body: bytes) -> str:
-    if "." in path:
-        ext = path.rsplit(".", maxsplit=1)[-1]
-    else:
-      ext = ""
-    content_type = self.MIME_TYPES.get(ext, "application/octet-stream")
+  def build_response_header(self, path: str, response_body: bytes, content_type: Optional[str]) -> str:
+    if content_type is None:
+      if "." in path:
+          ext = path.rsplit(".", maxsplit=1)[-1]
+      else:
+        ext = ""
+      content_type = self.MIME_TYPES.get(ext, "application/octet-stream")
 
     # generate response header
     response_header = ""
@@ -95,4 +146,3 @@ class WorkerThread(Thread):
     response_header += f"Content-Type: {content_type}\r\n"
 
     return response_header
-
